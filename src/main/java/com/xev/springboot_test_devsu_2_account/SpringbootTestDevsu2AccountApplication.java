60bc3cbbda4303079c49aae78e5b48803f27d6ca
DevSebastianGr <devsebastiangr@gmail.com>
2025-11-27 02:34:50 -0500

    Project Created

---
+++ src/main/java/com/xev/springboot_test_devsu_2_account/SpringbootTestDevsu2AccountApplication.java
@@ -0,0 +1,13 @@
+package com.xev.springboot_test_devsu_2_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestDevsu2AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestDevsu2AccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xev/springboot_test_devsu_2_account/SpringbootTestDevsu2AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xev.springboot_test_devsu_2_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestDevsu2AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
